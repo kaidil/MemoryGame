1b049188b2ef42dac25aff2317ed20696225ae31
Kaidi-Liis <kaidiliis.liim@ttu.ee>
2018-05-31 01:56:30 +0300

    Added score counting functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MemoryGame
+﻿# MemoryGame
 4. kodutoo aines eesrakenduste arendamine
 
 Töö autorid: Katri Palo, Kaidi-Liis Liim, Maritana Sampu
@@ -9,3 +9,5 @@ sulgetakse need uuesti. Kui kõik paarid on leitud, pakutakse uut mängu alustad
 Idee pärineb siit: https://www.youtube.com/watch?v=c_ohDPWmsM0
 Lisaks sellele toimub ka punktiarvestus, kus arvestatkse tehtud käikude arvu. Mida vähem käike, seda rohkem punkte.
 Mängu lõpus kuvatakse punktid ekraanile, neid ei salvestata.
+
+Mängu ekraanitõmmis pärast kaartide avamist: ![Screenshot 1](MemoryGame.png "Rakenduse ekraanipilt lõpus")
--- functions.js
+++ functions.js
@@ -2,7 +2,9 @@ var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H',
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
+var total_clicks = 0;
 
+// Kaartide segamise funktsioon, et saaks memory_arrayd iga aeg uuesti välja kutsuda
 Array.prototype.memory_tile_shuffle = function(){
     var i = this.length, j, temp;
     while(--i > 0){
@@ -23,8 +25,10 @@ function registerServiceWorker() {
     }
 }
 
+// F-n uue kaardilaua loomiseks
 function newBoard(){
     tiles_flipped = 0;
+    total_clicks = 0;
     var output = '';
     memory_array.memory_tile_shuffle();
     for(var i = 0; i < memory_array.length; i++){
@@ -33,28 +37,33 @@ function newBoard(){
     document.getElementById('memory_board').innerHTML = output;
 }
 
+// F-n uue kaardilaua loomiseks
 function memoryFlipTile(tile,val){
-    if(tile.innerHTML == "" && memory_values.length < 2){
+    total_clicks++;
+    console.log('total_clicks:', total_clicks);
+    document.getElementById("klicks").innerHTML = "Klikke: " + total_clicks;
+    if(tile.innerHTML == "" && memory_values.length < 2){   // kui kasutaja ei ole veel yhtegi kaarti avanud
         tile.style.background = '#FFF';
         tile.innerHTML = '<img src="./images/' + val + '.png"/>';
-        if(memory_values.length == 0){
+        if(memory_values.length == 0){  // hakkab jooksma alles siis kui kasutajal juba 1 kaart avatud
             memory_values.push(val);
             memory_tile_ids.push(tile.id);
-        } else if(memory_values.length == 1){
+        } else if(memory_values.length == 1){   // kontrollime, kas on paar
             memory_values.push(val);
             memory_tile_ids.push(tile.id);
             if(memory_values[0] == memory_values[1]){
                 tiles_flipped += 2;
                 // Clear both arrays
-                memory_values = [];
+                memory_values = []; // tyhjendame massiivid, et uut paari hakata otsima
                 memory_tile_ids = [];
                 // Check to see if the whole board is cleared
-                if(tiles_flipped == memory_array.length){
-                    alert("You won! Generating new board");
+                if(tiles_flipped == memory_array.length){   // kotroll, kas kas kõik kaardid avatud, kui jah, siis uus laud
+                    score = (24 / total_clicks) * 100;
+                    alert("Oled võitnud! Sinu skoor on: " + Math.round(score));
                     document.getElementById('memory_board').innerHTML = "";
                     newBoard();
                 }
-            } else {
+            } else {    // kui kasutaja ei saanud paari, siis jätkub kood siit
                 function flip2Back(){
                     // Flip the 2 tiles back over
                     var tile_1 = document.getElementById(memory_tile_ids[0]);
